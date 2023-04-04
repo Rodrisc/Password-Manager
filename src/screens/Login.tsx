4cad02015618a1337c04a4fdceeed54e849a427e
Rodrigo Soares <87383983+Rodrisc@users.noreply.github.com>
2023-04-04 20:14:04 -0300

    alteração da tipagem

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -4,7 +4,7 @@ import { Button } from '../components/Button';
 import Logo from '../assets/logosvg.svg'
 
 type Props = {
-    authenticate: any
+    authenticate: () => void
 }
 
 export function Login({ authenticate }: Props) {
