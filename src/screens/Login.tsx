2af5f8036b330e8d3517bc0416baa7656e2b263f
Rodrigo Soares <87383983+Rodrisc@users.noreply.github.com>
2023-06-04 16:53:37 -0300

    Estilizacao da tela de login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ImageBackground, Image, StyleSheet } from 'react-native'
 import { VStack, Text, useTheme, Heading } from 'native-base';
 import { Button } from '../components/Button';
 import Logo from '../assets/logosvg.svg'
@@ -9,24 +10,38 @@ type Props = {
 
 export function Login({ authenticate }: Props) {
 
-    const { colors } = useTheme()
+    // const { colors } = useTheme()
 
     return (
-        <VStack flex={1} alignItems='center' justifyContent='center'>
+        <ImageBackground source={require('../assets/imageBackground.jpg')} style={styles.ImageBackground}>
+            <VStack flex={1} alignItems='center' justifyContent='center'>
 
-            <Logo />
 
-            <Heading p={30}>
-                Password Manager
-            </Heading>
-            
 
-            <Text textAlign='center' p={2} fontSize='lg' pb={10}>
-                Para ter acesso ao Password Manager, entre com as credênciais do seu dispositivo.
-            </Text>
+                <Logo />
 
-            <Button title='Acessar' onPress={authenticate} w={20} />
+                <Heading p={30}>
+                    Password Manager
+                </Heading>
 
-        </VStack>
+
+                <Text textAlign='center' p={2} fontSize='lg' pb={10}>
+                    Para ter acesso ao Password Manager, entre com as credênciais do seu dispositivo.
+                </Text>
+
+                <Button title='Acessar' onPress={authenticate} w={20} borderRadius={30} />
+
+            </VStack>
+
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    ImageBackground: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
